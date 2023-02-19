4dc0aee2d6de7773d5f82d128ed74c3df4a6d90d
Hackle Wayne <hwayne@atlassian.com>
2023-02-20 11:24:55 +1300

    digit sum

--- app/Main.hs
+++ app/Main.hs
@@ -18,6 +18,7 @@ mergeN q | PQ.null q = []
                 q1 = PQ.deleteMin q & tryInsert xs
             in x:mergeN q1
 
+
 sortN :: Ord a => [[a]] -> [a]
 sortN = mergeN . foldr tryInsert PQ.empty
 
@@ -30,7 +31,7 @@ sortN = mergeN . foldr tryInsert PQ.empty
 -- Just 6
 -- ghci> floyd $ 1:2:3:4:5:(concat $ repeat [6..25])
 -- Just 6
-floyd :: Eq a => [a] -> Maybe a
+-- floyd :: Eq a => [a] -> Maybe a
 floyd xs = do
     met <- race1 xs xs
     race2 xs met
@@ -51,3 +52,28 @@ floyd1 xs = race1 xs xs >>= race2 xs
 floyd2 xs = race2 xs =<< race1 xs xs
 
 floyd3 xs = xs & (race1 xs >=> race2 xs)
+
+
+-- isSorted :: Ord a => [a] -> Bool
+isSorted xs = compareLists xs (tail xs)
+
+compareLists (x:xs) (y:ys) = x <= y && compareLists xs ys
+compareLists _ [] = True
+compareLists [] _ = False
+
+-- ghci> digitSum 1
+-- 1
+-- ghci> digitSum 12
+-- 3
+-- ghci> digitSum 123
+-- 6
+-- ghci> digitSum 59
+-- 5
+-- ghci> digitSum 181
+-- 1
+-- ghci> digitSum 148148148
+-- 3
+digitSum n  | n <= 9 = n
+            | otherwise = 
+                let (q, r) = n `divMod` 10
+                in  digitSum $ q + digitSum r
